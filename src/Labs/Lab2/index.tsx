7d75a6f55022f1c13c5632cdb605b751a8755f60
Caroline Pham <carolinetpham@outlook.com>
2024-09-24 13:40:29 -0400

    adding bootstrap components

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -12,10 +12,11 @@ import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
 import ReactIconsSampler from "./ReactIcons";
+import BootstrapGrids from "./BootstrapGrids";
 
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <div className="container">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <div id="wd-css-id-selectors">
@@ -75,6 +76,7 @@ export default function Lab2() {
       <GridLayout />
       <Flex />
       <ReactIconsSampler />
+      <BootstrapGrids />
     </div>
   );
 }
