b51e7a2535cfa087a3f68ebcd31cdbc00334185a
Caroline Pham <carolinetpham@outlook.com>
2024-10-17 13:28:09 -0400

    fixing labs path

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -10,7 +10,7 @@ export default function KanbasNavigation() {
     { label: "Courses", path: "/Kanbas/Dashboard", icon: LiaBookSolid },
     { label: "Calendar", path: "/Kanbas/Calendar", icon: IoCalendarOutline },
     { label: "Inbox", path: "/Kanbas/Inbox", icon: FaInbox },
-    { label: "Labs", path: "./Labs", icon: LiaCogSolid },
+    { label: "Labs", path: "/Labs", icon: LiaCogSolid },
   ];
 
   return (
